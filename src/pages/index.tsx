3e3930c82912fe79dddd5bd2de43d00c6b2a3cf3
Abhay <abhayonlyone@gmail.com>
2021-07-01 12:56:01 +0530

    Responsiveness fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,28 @@ import * as React from "react"
 
 import Layout from "../components/Layout"
 import Seo from "../components/seo"
+import { useMediaQuery } from "react-responsive"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1 className="heading">About</h1>
-    <p>I am Abhay, I am a developer</p>
-  </Layout>
-)
+import * as styles from "./index.module.css"
+
+const IndexPage = () => {
+  const isSmallScreen = useMediaQuery({ maxWidth: "950px" })
+  console.log(isSmallScreen)
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <section className={styles.section}>
+        <div className={styles.nameTitleHolder}>
+          <h5 className={styles.title}>
+            {!isSmallScreen && "DEVELOPER"} {isSmallScreen && "DEV"}
+          </h5>
+          <h2 className={styles.name}>Abhay Raj Singh</h2>
+        </div>
+      </section>
+      <h2 className="heading">About</h2>
+      <p>I am Abhay, I am a developer</p>
+    </Layout>
+  )
+}
 
 export default IndexPage
